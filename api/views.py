23ee489f7e628b84714fd04d14cd62b6333212c5
Jules Delobelle <jules.delobelle@edu.esiee.fr>
2026-05-13 16:54:43 +0200

    Création des vues

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Profil, Session, Detection
+from .serializers import ProfilSerializer, SessionSerializer, DetectionSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+
+class ProfilViewSet(viewsets.ModelViewSet):
+    queryset = Profil.objects.all()
+    serializer_class= ProfilSerializer
+
+
+class SessionViewSet(viewsets.ModelViewSet):
+    queryset = Session.objects.all()
+    serializer_class = SessionSerializer
+
+
+class DetectionViewSet(viewsets.ModelViewSet):
+    queryset = Detection.objects.all()
+    serializer_class = DetectionSerializer
