2808ae45223d44f7bf0a21cc13139be34b40bbb0
Jules Delobelle <jules.delobelle@edu.esiee.fr>
2026-06-12 11:11:38 +0200

    Modif: allowed_hosts final

--- empatheyebackend/settings.py
+++ empatheyebackend/settings.py
@@ -31,8 +31,11 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'valeur-par-defaut-locale')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False) == True
 
-ALLOWED_HOSTS = [   
-    '*'
+ALLOWED_HOSTS = [
+    'https://stingray-app-k65jv.ondigitalocean.app',
+    'localhost',
+    '127.0.0.1',
+    '10.0.2.2',
 ]
 
 
